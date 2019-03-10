cf2cd21eb17a22624c67e224116c1b040dad8ad7
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2019-03-10 15:43:34 +0100

    Initial commit with skeleton application and webpack-encore

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const Encore = require('@symfony/webpack-encore');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+Encore
+    .setOutputPath('web/build/')
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+    .enableSassLoader()
+
+    .enableReactPreset()
+
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    .enableVersioning(Encore.isProduction())
+
+    .addPlugin(new CopyWebpackPlugin([{
+        from: 'assets/img/',
+        to: 'img/'
+    }]))
+
+    .addPlugin(new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }))
+;
+
+module.exports = Encore.getWebpackConfig();
